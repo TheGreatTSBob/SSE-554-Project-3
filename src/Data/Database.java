1bf5bb2aaaa737bf4978f12935a39fb3afa04cb7
TheGreatTSBob <thegreattsbob@gmail.com>
2015-03-29 20:13:07 -0400

    Database testing and psuedocode

--- src/Data/Database.java
+++ src/Data/Database.java
@@ -3,7 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Data;
+package data;
+
+import banking.Account;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,4 +18,39 @@ package Data;
  */
 public class Database {
     
+    
+    protected Connection getConnection() throws SQLException, IOException
+    {
+     
+       String url = "jdbc:mysql://localhost:3306/sse554";
+       String username = "root";
+       String password = "password";
+       
+       return DriverManager.getConnection(url, username, password);
+    }
+    
+    public void writeToDatabase(ArrayList<Account> accounts)
+    {
+        /*
+        Do some cool stuff here
+        
+        Remove old table
+        Create new table with account's data
+        
+        */
+    }
+    
+    public ArrayList<Account> readFromDatabase()
+    {
+        ArrayList<Account> accounts = new ArrayList<>();
+        
+        /*
+        Do some cool stuff here too
+        
+        Select * from bank database
+        Add data for each account as an insert command
+        */
+        
+        return accounts;
+    }
 }
